325b5703415475c1dc9c47931c324809b7be2c2d
Jo Colina <jo@jocolina.com>
2023-02-07 10:03:54 +0100

    GitHub: migrate to Terraform v1.3 & add GitHub Pages

--- github-repo/terraform.tf
+++ github-repo/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
-  experiments = [module_variable_optional_attrs]
-
+  required_version = ">= 1.3.0"
+  # From terraform v1.3 no need for experiment
   required_providers {
     github = {
       source = "integrations/github"
