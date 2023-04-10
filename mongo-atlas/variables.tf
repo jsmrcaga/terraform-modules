f5e11f6980b3e56934e78a9b471f6434255e1a1e
Jo Colina <jo@jocolina.com>
2023-04-10 15:05:46 +0200

    Mongo Atlas: migrate to tf 1.4 (no more defaults)

--- mongo-atlas/variables.tf
+++ mongo-atlas/variables.tf
@@ -11,25 +11,25 @@ variable project {
       name = string
       organization_id = string
       owner_id = optional(string)
-      teams = optional(map(list(string)))
-      is_collect_database_specifics_statistics_enabled = optional(bool)
-      is_data_explorer_enabled = optional(bool)
-      is_performance_advisor_enabled = optional(bool)
-      is_realtime_performance_panel_enabled = optional(bool)
-      is_schema_advisor_enabled = optional(bool)
+      teams = optional(map(list(string)), {})
+      is_collect_database_specifics_statistics_enabled = optional(bool, true)
+      is_data_explorer_enabled = optional(bool, true)
+      is_performance_advisor_enabled = optional(bool, true)
+      is_realtime_performance_panel_enabled = optional(bool, true)
+      is_schema_advisor_enabled = optional(bool, true)
     })
 }
 
 variable cluster {
     type = object({
       name = string
-      type = optional(string)
-      auto_scaling = optional(bool)
-      mongo_version = optional(string)
-      plan = optional(string)
-      cloud_provider = optional(string)
-      backing_cloud_provider = optional(string)
-      cloud_region = optional(string)
+      type = optional(string, "REPLICA_SET")
+      auto_scaling = optional(bool, true)
+      mongo_version = optional(string, "5.0")
+      plan = optional(string, "M0")
+      cloud_provider = optional(string, "TENANT")
+      backing_cloud_provider = optional(string, "AWS")
+      cloud_region = optional(string, "EU_WEST_3")
     })
 }
 
