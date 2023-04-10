5393032f92c8b865d86daa8e6cdd7a8ae7bf4c16
Jo Colina <jo@jocolina.com>
2023-04-10 15:05:46 +0200

    Mongo Atlas: migrate to tf 1.4 (no more defaults)

--- mongo-atlas/database.tf
+++ mongo-atlas/database.tf
@@ -1,11 +1,11 @@
 resource "mongodbatlas_project" "project" {
   name = var.project.name
   org_id = var.project.organization_id
-  project_owner_id = local.project.owner_id
+  project_owner_id = var.project.owner_id
 
   # Build teams specification
   dynamic "teams" {
-    for_each = local.project.teams
+    for_each = var.project.teams
     iterator = each
 
     content {
@@ -14,11 +14,11 @@ resource "mongodbatlas_project" "project" {
     }
   }
 
-  is_collect_database_specifics_statistics_enabled = local.project.is_collect_database_specifics_statistics_enabled
-  is_data_explorer_enabled = local.project.is_data_explorer_enabled
-  is_performance_advisor_enabled = local.project.is_performance_advisor_enabled
-  is_realtime_performance_panel_enabled = local.project.is_realtime_performance_panel_enabled
-  is_schema_advisor_enabled = local.project.is_schema_advisor_enabled
+  is_collect_database_specifics_statistics_enabled = var.project.is_collect_database_specifics_statistics_enabled
+  is_data_explorer_enabled = var.project.is_data_explorer_enabled
+  is_performance_advisor_enabled = var.project.is_performance_advisor_enabled
+  is_realtime_performance_panel_enabled = var.project.is_realtime_performance_panel_enabled
+  is_schema_advisor_enabled = var.project.is_schema_advisor_enabled
 }
 
 # Very simple cluster config
@@ -28,16 +28,16 @@ resource "mongodbatlas_cluster" "db_cluster" {
   name = var.cluster.name
 
   # for some reason M0 clusters do not accept cluster_type (HTTP 500)
-  # cluster_type = local.cluster.type
+  # cluster_type = var.cluster.type
 
-  auto_scaling_disk_gb_enabled = local.cluster.auto_scaling
-  mongo_db_major_version = local.cluster.mongo_version
+  auto_scaling_disk_gb_enabled = var.cluster.auto_scaling
+  mongo_db_major_version = var.cluster.mongo_version
 
   # Provider
-  provider_instance_size_name = local.cluster.plan
-  provider_name = local.cluster.cloud_provider
-  backing_provider_name = local.cluster.backing_cloud_provider
-  provider_region_name = local.cluster.cloud_region
+  provider_instance_size_name = var.cluster.plan
+  provider_name = var.cluster.cloud_provider
+  backing_provider_name = var.cluster.backing_cloud_provider
+  provider_region_name = var.cluster.cloud_region
 }
 
 resource "mongodbatlas_database_user" "db_users" {
